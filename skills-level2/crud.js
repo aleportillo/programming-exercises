ca458bd9e9fcfffebd8330d56510534b49259de2
Freida Portillo <alejandrabypoca@gmail.com>
2024-01-30 01:46:21 -0700

    WIP(skills2) -  exercise 3

---
+++ skills-level2/crud.js
@@ -0,0 +1,26 @@
+import * as p from '@clack/prompts';
+import color from 'picocolors';
+import { sleep } from './index.js';
+const s = p.spinner();
+
+// Manchester United FC Player Management System:
+// As a developer for Manchester United FC, the executive management 
+// has tasked you with creating a CRUD system for the current players, 
+// including the following information: Jersey number, position, age, height,
+// and other statistical data. Additionally, integrate the system from 
+// the previous level where it was possible to compare two players and 
+// visualize their characteristics. You may find player information by 
+// searching on the internet.
+// Features:
+// - Create: Add new players to the system with their respective details.
+// - Read: View the complete list of current players with their 
+//   jersey number, position, age, height, and other statistical information.
+// - Update: Modify player information as needed, such as 
+//   position, age, or height.
+// - Delete:Remove players from the system if they are no
+//   longer part of the team.
+// - Compare Players: Utilize the comparison feature to analyze and 
+//   contrast the characteristics of two players.
+// - Visualize Characteristics: Display the statistical and 
+//   physical attributes of each player for a comprehensive overview.
+
